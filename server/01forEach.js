f87b14c3a924ab0f0d7362da1395ea487ac8203a
arieldelg <136664352+arieldelg@users.noreply.github.com>
2023-08-07 09:27:53 -0700

    //new changes forEach.html and forEach.js

--- server/01forEach.js
+++ server/01forEach.js
@@ -1,3 +1,15 @@
 const letters = ['a', 'b', 'c'];
 
-letters.forEach(element => console.log('for Each', element)) 
+letters.forEach(element => console.log('forEach', element)) 
+
+const div = document.getElementById('ap')
+
+const products = [
+    {title: 'Burro', price: '220 MX'},
+    {title: 'Cangerburguer', price: '280 MX'},
+];
+
+
+products.forEach(element => {
+    div.innerHTML += `<li>${element.title}: ${element.price}</li>`
+})
